44897d3ee30fd764f3a2a27ab579c298aa29806c
Dave Compton <davecompton7@gmail.com>
2016-05-14 20:34:35 +0000

    changed link representation of face to center symmetric

--- Main.hs
+++ Main.hs
@@ -1,43 +1,51 @@
 import Reflex.Dom
 
-data Color = Red | Green | Blue | Yellow | Orange | White deriving Show
+data Color = Red 
+           | Green 
+           | Blue 
+           | Yellow 
+           | Orange 
+           | Purple 
+           deriving Show
 
-data DWeb a = DWeb { north :: DWeb a
-                   , south :: DWeb a
-                   , west  :: DWeb a
-                   , east  :: DWeb a
-                   , val   :: a
-              }
+data DNode a = DNode { north :: DNode a
+                     , west :: DNode a
+                     , south  :: DNode a
+                     , east  :: DNode a
+                     , val   :: a
+                     }
 
-mkList :: DWeb a -> (DWeb a -> DWeb a) -> Int -> [DWeb a]
-mkList _ _ 0 = []
-mkList dw advance c = dw:mkList (advance dw) advance (c-1)
+type Facet = DNode Color
 
-mkWeb :: [DWeb a] -> [DWeb a] -> [DWeb a] -> [DWeb a] -> Int -> Int -> a -> ([DWeb a], [DWeb a], [DWeb a], [DWeb a])
-mkWeb ns ss ws es rowCount colCount v = 
-    let (w, e) = mkWeb0 ns ss ws es rowCount colCount v
-    in (mkList (head w) east colCount, mkList (head e) west colCount, w, e)
+type Edge = (Facet,Facet,Facet)
 
-mkWeb0 :: [DWeb a] -> [DWeb a] -> [DWeb a] -> [DWeb a] -> Int -> Int -> a -> ([DWeb a], [DWeb a])
-mkWeb0 _ _ ws es _ 0 _ = (es, ws)
-mkWeb0 (n:ns) (s:ss) ws es rowCount colCount v = 
-    let this = mkChain n s ws rest rowCount v
-        (rest, last) = mkWeb0 ns ss this es rowCount (colCount-1) v
-    in (this, last)
+mkDomino :: Facet -> Facet -> Facet -> Facet -> Facet -> Facet -> Color -> (Facet,Facet)
+mkDomino right upperRight upperLeft left lowerLeft lowerRight color =
+    let dominoRight = DNode upperRight dominoLeft lowerRight right       color
+        dominoLeft =  DNode upperLeft  left       lowerLeft  dominoRight color
+    in (dominoLeft, dominoRight)
 
-mkChain :: DWeb a -> DWeb a -> [DWeb a] -> [DWeb a] -> Int -> a -> [DWeb a]
-mkChain n s ws es rowCount v = 
-    let (f,_) = mkChain0 n s ws es rowCount v
-    in mkList f south rowCount
+mkFace :: Edge -> Edge -> Edge -> Edge -> Color -> (Edge,Edge,Edge,Edge)
+mkFace (nRight, nCenter, nLeft) 
+       (wRight, wCenter, wLeft) 
+       (sRight, sCenter, sLeft) 
+       (eRight, eCenter, eLeft) 
+       color =
 
-mkChain0 :: DWeb a -> DWeb a -> [DWeb a] -> [DWeb a] -> Int -> a -> (DWeb a, DWeb a)
-mkChain0 n s _ _ 0 v = (s,n)
-mkChain0 n s (w:ws) (e:es) rowCount v = 
-    let this = DWeb n rest w e v 
-        (rest, last) = mkChain0 this s ws es (rowCount-1) v
-    in (this, last)
+    let center = DNode nSide wSide sSide eSide color
+        (nwCorner, nSide) = mkDomino enCorner nCenter nLeft wRight wSide center color
+        (wsCorner, wSide) = mkDomino nwCorner wCenter wLeft sRight sSide center color
+        (seCorner, sSide) = mkDomino wsCorner sCenter sLeft eRight eSide center color
+        (enCorner, eSide) = mkDomino seCorner eCenter eLeft nRight nSide center color
 
-data Model = Model { cube :: DWeb Color }
+    in ( (nwCorner, nSide, enCorner)
+       , (wsCorner, wSide, nwCorner)
+       , (seCorner, sSide, wsCorner)
+       , (enCorner, eSide, seCorner)
+       )
+
+
+data Model = Model { cube :: Facet }
 
 showFace f = do 
                 let nf = north f
@@ -54,24 +62,13 @@ showFace f = do
                 text $ show $ val sf 
                 text $ show $ val $ east sf 
 
+main = 
+          let  (nPurple, wPurple, sPurple,  ePurple) =   mkFace nGreen    nBlue     nYellow   nRed       Purple
 
-main = mainWidget $ do
-           let d = 3
-               (b0n, b0s, b0w, b0e) = mkWeb (reverse b3e) b1e b2e (reverse b4e) d d Yellow
+               (nYellow, wYellow, sYellow,  eYellow) =   mkFace sPurple   eBlue     nOrange   wRed       Yellow
+               (nBlue,   wBlue,   sBlue,    eBlue)   =   mkFace wPurple   eGreen    wOrange   wYellow    Blue
+               (nGreen,  wGreen,  sGreen,   eGreen)  =   mkFace nPurple   eRed      sOrange   wBlue      Green
+               (nRed,    wRed,    sRed,     eRed)    =   mkFace ePurple   eYellow   eOrange   wGreen     Red
 
-               (b1n, b1s, b1w, b1e) = mkWeb b2s b4n b5n b0s d d Red
-               (b2n, b2s, b2w, b2e) = mkWeb b3s b1n (reverse b5w) b0w d d Green
-               (b3n, b3s, b3w, b3e) = mkWeb b4s b2n (reverse b5s) (reverse b0n) d d Blue
-               (b4n, b4s, b4w, b4e) = mkWeb b1s b3n b5e (reverse b0e) d d Orange
-
-               (b5n, b5s, b5w, b5e) = mkWeb b1w (reverse b3w) (reverse b2w) b4w d d White
-
-               yellowCorner = head b0n
-               yellowCenter = south $ east yellowCorner 
-
-               redCorner = head b1n
-               redCenter = south $ east redCorner 
-
-
-           el "div" $ showFace yellowCenter
-           el "div" $ showFace redCenter
+               (nOrange, wOrange, sOrange, eOrange)  =   mkFace sYellow   sBlue     sGreen    sRed       Orange
+          in mainWidget $ text "hello"
